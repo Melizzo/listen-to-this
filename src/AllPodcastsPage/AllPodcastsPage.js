72f8fd50ca5f17c8b294b74f97f192f4669906c5
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-29 14:23:57 -0600

    :tada:initial commit

---
+++ src/AllPodcastsPage/AllPodcastsPage.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+const AllPodcastsPage = (props) => {
+  const [givenPodcasts, setGivenPodcast] = useState
+
+  // useEffect(() => {setGivenPodcast(props.givenPodcasts)})
+
+  const podcastCards = Object.values(givenPodcasts).map((podcast) => {
+    return (
+      <PodcastDetailsCard 
+        // props.title
+        // props.img
+        // props.description
+      />
+    )
+  })
+  
+
+  return (
+    <section className="All-podcasts-container">
+      {/* add in error via props 
+      	{props.error && 
+				<div className='error-msg'>404: No Podcasts found</div>
+			}
+      */}
+      {!props.error && podcastCards}
+      {!props.error && !podcastCards && <h3 className="no-searched-podcasts">Search for a podcast above! Enter in a topic that interests you.</h3>}
+    </section>
+  )
+}
+
+
+export default AllPodcastsPage
