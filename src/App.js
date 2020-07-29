44d718492b19b7ba140218c4b1d8cce96fc9d964
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-29 10:54:36 -0600

    :crystal_ball: create file structure for Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from './Header'
 import './App.css';
 
 function App() {
   return (
     <main>
-      
+      <Header />
     </main>
   );
 }
