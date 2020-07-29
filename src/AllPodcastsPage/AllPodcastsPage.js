131fc87092b7345589411bdd5bfe1ad0bdb837a7
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-29 14:08:59 -0600

    :crystal_ball: created basic outline for All Podcasts Page

--- src/AllPodcastsPage/AllPodcastsPage.js
+++ src/AllPodcastsPage/AllPodcastsPage.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AllPodcastsPage = () => {
+const AllPodcastsPage = (props) => {
+  const [givenPodcasts, setGivenPodcast] = useState
+
+  const podcastCards = Object.values(givenPodcasts).map((podcast) => {
+    return (
+      <PodcastDetailsCard 
+        // props.title
+        // props.img
+        // props.description
+      />
+    )
+  })
+  
 
   return (
-    <PodcastDetailsCard 
-    
-    />
+    <section className="All-podcasts-container">
+      {/* add in error via props */}
+      {!props.error && podcastCards}
+      {!props.error && !podcastCards && <h3 className="no-searched-podcasts">Search for a podcast above! Enter in a topic that interests you.</h3>}
+    </section>
   )
 
 }
