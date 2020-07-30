10447f7fb61833087d07f3470d65b868062dd148
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-30 13:48:07 -0600

    :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./Header/Header";
 import AllPodcastsPage from "./AllPodcastsPage/AllPodcastsPage"
-import PodcastDetails from './'
+import PodcastDetails from './PodcastDetails/PodcastDetails'
 import { getSearchedPodcasts } from './ApiCalls'
 import "./App.css";
 
@@ -31,7 +31,7 @@ const App = () => {
         <Switch>
         <Route 
           path="/podcast/:id"
-          render={({ match}) => {
+          render={({ match }) => {
             const { id } = match.params;
             return(
               <PodcastDetails 
