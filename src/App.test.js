b0e972c26d8b2961264a6fb2350a8f8f0bf71608
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 16:18:02 -0600

    :bar_chart: resolved error for mock data

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+import { mock } from '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import { render, fireEvent, waitFor } from '@testing-library/react'
 import { getSearchedPodcasts, getPodcastDetails } from './ApiCalls.js'
 import TestSearchAll from './testResponses/TestAPISearchVegan'
 import TestSearchedPodcast from './testResponses/TestPodcastDetails.js'
 jest.mock('./ApiCalls.js')
+import MutationObserver from '@sheerun/mutationobserver-shim'
+window.MutationObserver = MutationObserver
 
 describe('App', () => {
   beforeEach(() => {
-    mocked(getSearchedPodcasts).mockImplementation(() =>
+    getSearchedPodcasts.mockImplementation(() =>
       Promise.resolve([
         {
           TestSearchAll
@@ -17,7 +21,7 @@ describe('App', () => {
       ])
     )
     
-    mocked(testPodcastDetails).mockImplementation(() => 
+    getPodcastDetails.mockImplementation(() => 
       Promise.resolve([
         {
           TestSearchedPodcast
@@ -26,5 +30,11 @@ describe('App', () => {
     )
   })
 
-  it('should render the correct content', () => {})
+  it('be able to search for podcasts and display results', async () => {
+    const { getByText } = render(
+      <MemoryRouter><App /></MemoryRouter>
+    )
+
+    await waitFor(() => expect(getSearchedPodcasts).toHaveBeenCalled())
+  })
 })
