ce01450042af7f0f9034346c6650760d73019d75
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 12:22:39 -0600

    :bar_chart: completed unit test for header render

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import Header from './Header'
-import PodcastDetails from './PodcastDetails'
-import "@testing-library/jest-dom"
-import {render, waitFor, fireEvent, getByLabelText, getByAltText } from "@testing-library/react"
-import { MemoryRouter } from "react-router-dom"
-import { getPodcastDetails } from '../ApiCalls'
-jest.mock("../ApiCalls.js")
+import React from "react";
+import Header from "./Header";
+import "@testing-library/jest-dom";
+import {
+  render,
+  waitFor,
+  fireEvent,
+  getByLabelText,
+  getByAltText,
+} from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { getPodcastDetails } from "../ApiCalls";
+jest.mock("../ApiCalls.js");
 
-describe('Header', () => {
-  it('should render the correct content', () => {
-    const  { getByText } = render(
+describe("Header", () => {
+  it("should render the correct content", () => {
+    const { getByText, getByLabelText } = render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>
-    )
-      const title = getByText('Listen To This!')
-      expect(title).toBeInTheDocument()
-  })
-})
+    );
+    const title = getByText("Listen To This!");
+    const input = getByLabelText("search");
+    const button = getByText('Search');
+    const favPodcastButton = getByText('View Saved Podcasts')
+
+    expect(title).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(favPodcastButton).toBeInTheDocument();
+  });
+
+  
+});
