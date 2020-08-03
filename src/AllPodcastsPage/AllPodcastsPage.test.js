143bd40a2678a42b107eb1ab9ddeca3d41ab0f50
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 13:28:30 -0600

    :tada: initial commit

--- src/AllPodcastsPage/AllPodcastsPage.test.js
+++ src/AllPodcastsPage/AllPodcastsPage.test.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import PodcastDetails from './PodcastDetails'
+import "@testing-library/jest-dom"
+import {render, waitFor, fireEvent, getByLabelText } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import { getPodcastDetails } from '../ApiCalls'
+jest.mock("../ApiCalls.js")
 
 describe('example', () => {
   it('should render the correct content', () => {})
