737ad78d4598cdcadb3f1772d4fb704da66030a2
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 16:34:03 -0600

    :art: flex bok added to heard

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -35,14 +35,10 @@ const Header = ({ findResults }) => {
             Search
           </button>
         </Link>
-        <Link to="/random_podcast"></Link>
-        <Link to="/favoritePodcasts">
-          <button className="header-search-button">View Saved Podcasts</button>
-        </Link>
-        <Link>
-          <h3 className="navlink-title">Random Podcast</h3>
-        </Link>
       </form>
+      <Link to="/favoritePodcasts">
+        <button className="header-search-button">View Saved Podcasts</button>
+      </Link>
     </header>
   );
 };
