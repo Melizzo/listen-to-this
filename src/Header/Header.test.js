9814870754597f8d651bf7f3ccd456914f3e26d0
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 12:47:15 -0600

    :bar_chart: completed unit test to verify search input

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,15 +1,9 @@
 import React from "react";
 import Header from "./Header";
 import "@testing-library/jest-dom";
-import {
-  render,
-  waitFor,
-  fireEvent,
-  getByLabelText,
-  getByAltText,
-} from "@testing-library/react";
+import { render, fireEvent, getAllByPlaceholderText } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { getPodcastDetails } from "../ApiCalls";
+import { getSearchedPodcasts } from "../ApiCalls";
 jest.mock("../ApiCalls.js");
 
 describe("Header", () => {
@@ -21,8 +15,8 @@ describe("Header", () => {
     );
     const title = getByText("Listen To This!");
     const input = getByLabelText("search");
-    const button = getByText('Search');
-    const favPodcastButton = getByText('View Saved Podcasts')
+    const button = getByText("Search");
+    const favPodcastButton = getByText("View Saved Podcasts");
 
     expect(title).toBeInTheDocument();
     expect(input).toBeInTheDocument();
@@ -30,5 +24,44 @@ describe("Header", () => {
     expect(favPodcastButton).toBeInTheDocument();
   });
 
-  
+  it('should allow a user to input a search keyword', () => {
+    const { getByPlaceholderText } = render(
+      <MemoryRouter>
+        <Header  />
+      </MemoryRouter>
+    )
+
+    const searchInput = getByPlaceholderText('search for a podcast')
+    fireEvent.change(searchInput, {target: {value: 'cats'}});
+    expect(searchInput).toHaveValue('cats')
+  })
+
+  // it("should click submit button", () => {
+  //   const mockFind = jest.fn();
+  //   const { getByText } = render(
+  //     <MemoryRouter>
+  //       <Header findResults={mockFind} />
+  //     </MemoryRouter>
+  //   );
+  //   const button = getByText("Search");
+
+  //   fireEvent.click(button);
+
+  //   expect(mockFind).toBeCalled();
+  // });
+
+  // it("should click favorite podcast button", () => {
+  //   const mockFind = jest.fn();
+  //   const { getByText, getByLabelText } = render(
+  //     <MemoryRouter>
+  //       <Header findResults={mockFind} />
+  //     </MemoryRouter>
+  //   );
+  //   const button = getByText("Search");
+
+  //   fireEvent.click(button);
+
+  //   expect(mockFind).toBeCalled();
+  // });
+
 });
