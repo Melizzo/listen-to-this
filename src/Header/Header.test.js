e849b3381fbad6a399f0da0cb4d4a52866487f28
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 12:09:52 -0600

    :tada: initial commit

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import Header from './Header'
+import PodcastDetails from './PodcastDetails'
+import "@testing-library/jest-dom"
+import {render, waitFor, fireEvent, getByLabelText, getByAltText } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import { getPodcastDetails } from '../ApiCalls'
+jest.mock("../ApiCalls.js")
 
-describe('example', () => {
-  it('should render the correct content', () => {})
+describe('Header', () => {
+  it('should render the correct content', () => {
+    const  { getByText } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    )
+      const title = getByText('Listen To This!')
+      expect(title).toBeInTheDocument()
+  })
 })
