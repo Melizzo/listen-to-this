26bae07697c5e546bb3eca2e844702ed7ef7a14d
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-08-03 13:41:00 -0600

    :tada: initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import { MemoryRouter } from 'react-router-dom'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+export const getSearchedPodcasts = (searchValue) => {
+import { getSearchedPodcasts, getPodcastDetails } from './ApiCalls.js'
+jest.mock('./ApiCalls.js')
 
-describe('example', () => {
+describe('App, () => {
+  beforeEach(() => {
+    mocked()
+  })
   it('should render the correct content', () => {})
 })
